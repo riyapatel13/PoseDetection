b056114bfa67d49ec8bad73cad69eb0e0c5d7552
Riya Patel <riaupatel13@yahoo.com>
2020-08-26 11:37:07 -0400

    added documentation to the code

--- hand_detect.py
+++ hand_detect.py
@@ -6,15 +6,24 @@ import json
 import csv
 import argparse
 
+'''
+hand_detect.py
+
+This file contains code for detecting points in time when the driver took his hands 
+off the wheel. After passing through the video in OpenPose and receiving a folder 
+of JSON files, this code reads through each file, determines which person in the 
+frame was the driver, and determines whether or not their hands were off the wheel
+using bounding boxes. This is the first "brute force" attempt at pose detection.
+'''
+
 # file paths
 json_folder = 'data/driving_jsons'
 vid_path = 'data/videos/output.mp4'
+# bounding box of driver (so we don't detect passengers)
 subject_rect = [(71,55), (243, 235)]
-
-# Data from finding rectangles using labelImg - saved as xml file 
+# Data from finding rectangles using labelImg - saved in annotated_driving.xml 
 # 8 rects = 8 lists
 # [top left, bottom right] where each pt is (x,y)
-# move with global vars
 wheel_rects_diags = [[(157,153),(165,165)],
             [(160,143),(167,152)],
             [(162,132),(174,142)],
@@ -24,7 +33,8 @@ wheel_rects_diags = [[(157,153),(165,165)],
             [(156,120),(173,163)],
             [(157,108),(201,123)]]
 
-# gets width and height of video
+
+# gets width and height of video (for debugging purposes)
 def vid_dimensions(vid_path):
     cap = cv2.VideoCapture(vid_path)
     width = int(cap.get(3))
@@ -33,16 +43,16 @@ def vid_dimensions(vid_path):
     return width, height
 
 
-# importing the JSON files
+# importing the JSON files into a list
 def import_jsons(json_folder_path):
     json_files = [json for json in os.listdir(json_folder_path) if json.endswith('.json')]
     return json_files
 
 
 '''
-Given folder of json files, boolean for whether we are looking at the left wrist, 
-and the number of frames to process (< number of files in json folder), this 
-function will create a dataframe with a columns as x, y, confidence. This data 
+Given a path to a folder of json files, boolean for whether we are looking at the 
+left wrist, and the number of frames to process (< number of files in json folder), 
+this function will create a dataframe with a columns as x, y, confidence. This data 
 was found by running the video through OpenPose.
 '''
 def create_dataframe(json_folder, is_left, num_frames):
@@ -58,7 +68,6 @@ def create_dataframe(json_folder, is_left, num_frames):
 
         # we know the subject's location on screen, so we can make sure to add points in that area to make sure we don't analyze the wrong person
         # [x, y, confidence]
-       
         if is_left:
                 for j in range(num_people_in_frame):
                     key_points = temp_df['people'][j]['pose_keypoints_2d']
@@ -89,7 +98,6 @@ def create_dataframe(json_folder, is_left, num_frames):
         df.fillna(0)
 
         try:
-            prev_df = df
             wrist_df = wrist_df.append(df)
         except:
             if is_left:
@@ -97,7 +105,7 @@ def create_dataframe(json_folder, is_left, num_frames):
             else:
                 print('bad right wrist point set at: ', file)
  
-        # cut off at num_frames frames because it takes too long
+        # cut off at num_frames frames because it takes too long to processes everything
         i+=1
         if i == num_frames:
             break
@@ -126,8 +134,12 @@ def scale_coords(coordinates, scale_factor):
             rect[i] = new_coord
 
 
-# given coordinates, it will return True if the point lies between the
-# bounds of any rectangle, False otherwise
+'''
+Given the dataframe of the wrist coordinates, a boolean for whether its the left 
+or right wrist, the top left and bottom right points of the bounding boxes, and
+a buffer (a buffer of 10 is 10 pixels around the bounding box), this function
+returns a list of all times the hands were outside of the bounding box
+'''
 def off_wheel_frames(wrist_df, is_left, rect_diags, buffer):
     hands_off_frames = []
     frame_num = 0
@@ -143,6 +155,7 @@ def off_wheel_frames(wrist_df, is_left, rect_diags, buffer):
         in_rect = False
 
         for rect in rect_diags:
+            # checking if in any rectangle
             if (x >= rect[0][0]-buffer and x <= rect[1][0]+buffer) and (y >= rect[0][1]-buffer and y <= rect[1][1]+buffer):
                 in_rect = True
             else:
@@ -158,12 +171,11 @@ def off_wheel_frames(wrist_df, is_left, rect_diags, buffer):
         frame_num += 1
     return hands_off_frames
 
-
 '''
 Given the list of frames where the hand is not on the wheel, this function will
 only keep the frames where its neighboring frames are within a range of buffer.
 i.e.: if buffer is 5 and the frame number is 60, it will keep frame 60 if there 
-exists a frame in the range of 55-65.
+exists a frame in the range of 55-65. This prevents noisy data from being added.
 '''
 def debounce_data(frames, buffer):
     filtered_frames = []
@@ -182,9 +194,11 @@ def debounce_data(frames, buffer):
 
 
 '''
-Given a list of frames and a range, this function will create a list of tuples of start to end frames in that range.
+Given a list of frames and a range, this function will create a list of tuples of 
+start to end frames in that range.
 ex: create_range([[7, 10, 15, 31, 35, 37, 51, 60, 78], 15)
-    --> [(7,15), (31,37), (51,60), (78,78)]
+    --> [(7,15), (31,60), (78,78)]
+The difference between the start of one tuple and the end of another is >= range.
 '''
 def create_ranges(frames, range_len):
     range_list = []
@@ -204,6 +218,7 @@ def create_ranges(frames, range_len):
     range_list.append(cur_range)
     return range_list
 
+
 # converts frame number to time in min:sec.microsecs given frames per sec of vid in string format
 def frame_to_time(frame_num, fps):
     total_secs = frame_num/fps
@@ -213,6 +228,7 @@ def frame_to_time(frame_num, fps):
     time = f"{mins}:{secs}"
     return time
 
+# writes text (list) as a list of JSON objects into outfile
 def write_file(text, is_left, outfile):
     data = {}
     json_list = []
@@ -222,8 +238,11 @@ def write_file(text, is_left, outfile):
             data["event"] = "left hand off wheel"
         else:
             data["event"] = "right hand off wheel"
-        data["start"] = frame_to_time(start, 15)
-        data["end"] = frame_to_time(end, 15)
+        
+        # change this depending on fps of video
+        fps = 15
+        data["start"] = frame_to_time(start, fps)
+        data["end"] = frame_to_time(end, fps)
         json_dump = json.dumps(data)
         json_list.append(json_dump)
 
@@ -245,13 +264,20 @@ if __name__ == "__main__":
 
     args = argparser.parse_args()
 
-    left_wrist_df = create_dataframe(args.input_folder, True, 1000)
-    right_wrist_df = create_dataframe(args.input_folder, False, 1000)
+    # change this if you want to analyze more/fewer frames
+    num_frames_to_analyze = 1000
+    left_wrist_df = create_dataframe(args.input_folder, True, num_frames_to_analyze)
+    right_wrist_df = create_dataframe(args.input_folder, False, num_frames_to_analyze)
 
-    l_hands_off_frames = off_wheel_frames(left_wrist_df, True, wheel_rects_diags, 10)
-    l_updated_frames = debounce_data(l_hands_off_frames, 5)
-    r_hands_off_frames = off_wheel_frames(right_wrist_df, False, wheel_rects_diags, 10)
-    r_updated_frames = debounce_data(r_hands_off_frames, 5)
+    # smaller padding --> tighter rects, larger padding --> bigger rects
+    rect_padding = 10
+    l_hands_off_frames = off_wheel_frames(left_wrist_df, True, wheel_rects_diags, rect_padding)
+    r_hands_off_frames = off_wheel_frames(right_wrist_df, False, wheel_rects_diags, rect_padding)
+
+    # smaller noise_buf --> more noisy data, larger noise_buf --> smoother data
+    noise_buf = 5
+    l_updated_frames = debounce_data(l_hands_off_frames, noise_buf)
+    r_updated_frames = debounce_data(r_hands_off_frames, noise_buf)
 
     # uncomment to see results
     '''
@@ -269,9 +295,11 @@ if __name__ == "__main__":
     print(r_updated_frames)
     print("Num frames: ",len(r_updated_frames),"\n")
     '''
-
-    l_hand_ranges = create_ranges(l_hands_off_frames, 15)
-    r_hand_ranges = create_ranges(r_hands_off_frames, 15)
+    # smaller time_range --> more precise results, larger --> less precise
+    # time_range is in frames
+    time_range = 15 # 1 s
+    l_hand_ranges = create_ranges(l_hands_off_frames, time_range)
+    r_hand_ranges = create_ranges(r_hands_off_frames, time_range)
 
     write_file(l_hand_ranges, True, args.output_file)
     write_file(r_hand_ranges, False, args.output_file)
